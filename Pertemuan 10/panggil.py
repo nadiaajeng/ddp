02b5d3eaf010085286facac2ab04b579938295ea
nadiaajeng <nadiaajss12@gmail.com>
2025-11-26 11:22:35 +0700

    Add files via upload

---
+++ Pertemuan 10/panggil.py
@@ -0,0 +1,23 @@
+import bangunRuang as bR
+import bangunDatar as bD
+
+print("----- BANGUN RUANG -----")
+
+print(bR.kubus(9))
+print(bR.balok(2,3,4))
+print(bR.prisma(10,24,15))
+print(bR.tabung(2,5))
+print(bR.kerucut(3,5))
+
+print("----- BANGUN DATAR -----")
+
+print(bD.persegi(4))
+print(bD.kel_persegi(5))
+print(bD.persegi_panjang(6,7))
+print(bD.kel_persegi_panjang(2,3))
+print(bD.lingkaran(1))
+print(bD.kel_lingkaran(2))
+print(bD.segitiga(6,7))
+print(bD.kel_segitiga(14,16,17))
+print(bD.jajargenjang(2,4))
+print(bD.kel_jajargenjang(3,4))
